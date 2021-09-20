762b9cc2e8acfa90e74e8452f2095aea923bba96
phenix995 <44148051+phenix995@users.noreply.github.com>
2021-09-20 10:16:43 -0400

    add neofetch package
    -add neofetch package
    -add qemu-guest-agent

--- install-proxmox-server.sh
+++ install-proxmox-server.sh
@@ -1,4 +1,4 @@
 apt update
 apt install qemu-guest-agent qemu-utils
 systemctl start qemu-guest-agent
-apt install -y vim curl git htop bpytop wget tmux vim
+apt install -y vim curl git htop bpytop wget tmux vim neofetch 
--- install-vm-basic.sh
+++ install-vm-basic.sh
@@ -1,2 +1,4 @@
 apt update
-apt install -y vim curl git htop bpytop wget tmux vim iptables 
+apt install qemu-guest-agent qemu-utils
+systemctl start qemu-guest-agent
+apt install -y vim curl git htop bpytop wget tmux vim iptables neofetch 
