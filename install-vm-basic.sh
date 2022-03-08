0eaa36b7940adea67512a9f08a71cb270b30381f
phenix995 <44148051+phenix995@users.noreply.github.com>
2022-03-08 13:11:58 -0500

    update

--- install-vm-basic.sh
+++ install-vm-basic.sh
@@ -2,5 +2,5 @@ apt update
 apt install -y sudo
 apt install -y qemu-guest-agent qemu-utils
 systemctl start qemu-guest-agent
-apt install -y vim curl git htop bpytop wget tmux neofetch sudo ufw fail2ban
+apt install -y vim curl git htop bpytop wget tmux neofetch sudo ufw fail2ban net-tools
 usermod -aG sudo debian
