11fd88ef5303bdd5ee4812163871b72d1510da8c
phenix995 <44148051+phenix995@users.noreply.github.com>
2022-05-25 23:59:59 -0400

    Create chmod.sh

---
+++ chmod.sh
@@ -0,0 +1,6 @@
+chmod +x update.sh
+chmod +x sense-hat.sh
+chmod +x install-vm-basic.sh
+chmod +x install-proxmox-server.sh
+chmod +x minecraft/install-srvminecraft-debian.sh
+chmod +x minecraft/install-srv-minecraft-part2-debian.sh
