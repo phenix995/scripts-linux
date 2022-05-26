e5102d425c554a248b8c653389635e64f77e6d91
phenix995 <44148051+phenix995@users.noreply.github.com>
2022-05-26 00:43:47 -0400

    Delete chmod.sh

--- chmod.sh
+++
@@ -1,6 +0,0 @@
-chmod +x update.sh
-chmod +x sense-hat-install.sh
-chmod +x install-vm-basic.sh
-chmod +x install-proxmox-server.sh
-chmod +x minecraft/install-srvminecraft-debian.sh
-chmod +x minecraft/install-srv-minecraft-part2-debian.sh
