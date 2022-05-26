9cdd3ddf9ae007d15817a8f6aaa45a6489a2fc16
phenix995 <44148051+phenix995@users.noreply.github.com>
2022-05-25 23:23:05 -0400

    add comment and remove dulplication install

--- install-vm-basic.sh
+++ install-vm-basic.sh
@@ -1,6 +1,7 @@
+#run as root
 apt update
 apt install -y sudo
 apt install -y qemu-guest-agent qemu-utils
 systemctl start qemu-guest-agent
-apt install -y vim curl git htop bpytop wget tmux neofetch sudo ufw fail2ban net-tools cifs-utils
+apt install -y vim curl git htop bpytop wget tmux neofetch ufw fail2ban net-tools cifs-utils
 usermod -aG sudo debian
