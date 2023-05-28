e5e25ca04d7026be62695091e896b38ffec023e7
Mathieu <44148051+phenix995@users.noreply.github.com>
2023-05-28 19:45:18 -0400

    fix

--- crontab-add-job.sh
+++ crontab-add-job.sh
@@ -1,8 +1,9 @@
 #write out current crontab
+touch temp-crontab
 crontab -l > temp-crontab
 
 #echo new cron into cron file
-echo "00 09 * * 1-5 echo hello" >> temp-crontab
+echo "00 09 * * 1-5 echo hello" > temp-crontab
 
 #install new cron file
 crontab temp-crontab
--- crontab-reconnect-bluetooth.sh
+++ crontab-reconnect-bluetooth.sh
@@ -1,8 +1,9 @@
 #write out current crontab
+touch temp-crontab
 crontab -l > temp-crontab
 
 #echo new cron into cron file
-echo "*/5 * * * * (bluetoothctl info 'AA:BB:CC:DD:EE:FF' | grep -q "Connected: yes" || echo -e 'connectAA\BB CCbDDeEEoFFctl)" >> temp-crontab
+echo "*/5 * * * * (bluetoothctl info 'AA:BB:CC:DD:EE:FF' | grep -q "Connected: yes" || echo -e 'connect AA:BB:CC:DD:EE:FF\n' | bluetoothctl)" > temp-crontab
 
 #install new cron file
 crontab temp-crontab
