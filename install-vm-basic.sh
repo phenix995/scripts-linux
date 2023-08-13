5c7bf6b46f63ab517565818985fa32a6975add47
Mathieu <44148051+phenix995@users.noreply.github.com>
2023-08-13 14:05:16 -0400

    create installation script for teslamate

--- install-vm-basic.sh
+++ install-vm-basic.sh
@@ -3,5 +3,5 @@ apt update
 apt install -y sudo
 apt install -y qemu-guest-agent qemu-utils
 systemctl start qemu-guest-agent
-apt install -y vim curl git htop bpytop wget tmux neofetch ufw fail2ban net-tools cifs-utils
+apt install -y vim curl git htop bpytop wget tmux neofetch ufw fail2ban net-tools cifs-utils nala
 usermod -aG sudo debian
---
+++ teslamate-install.sh
@@ -0,0 +1,65 @@
+apt update
+apt upgrade -y
+
+#apt install -y git tmux net-tools nala docker docker-compose-plugin qemu-guest-agent qemu-utils
+
+touch docker-compose.yml
+echo "version: "3"
+
+services:
+  teslamate:
+    image: teslamate/teslamate:latest
+    restart: always
+    environment:
+      - Spectral1965= #insert a secure key to encrypt your Tesla API tokens
+      - DATABASE_USER=teslamate
+      - Spectral1965= #insert your secure database password!
+      - DATABASE_NAME=teslamate
+      - DATABASE_HOST=database
+      - MQTT_HOST=mosquitto
+    ports:
+      - 4000:4000
+    volumes:
+      - ./import:/opt/app/import
+    cap_drop:
+      - all
+
+  database:
+    image: postgres:15
+    restart: always
+    environment:
+      - POSTGRES_USER=teslamate
+      - Spectral1965= #insert your secure database password!
+      - POSTGRES_DB=teslamate
+    volumes:
+      - teslamate-db:/var/lib/postgresql/data
+
+  grafana:
+    image: teslamate/grafana:latest
+    restart: always
+    environment:
+      - DATABASE_USER=teslamate
+      - Spectral1965= #insert your secure database password!
+      - DATABASE_NAME=teslamate
+      - DATABASE_HOST=database
+    ports:
+      - 3000:3000
+    volumes:
+      - teslamate-grafana-data:/var/lib/grafana
+
+  mosquitto:
+    image: eclipse-mosquitto:2
+    restart: always
+    command: mosquitto -c /mosquitto-no-auth.conf
+    # ports:
+    #   - 1883:1883
+    volumes:
+      - mosquitto-conf:/mosquitto/config
+      - mosquitto-data:/mosquitto/data
+
+volumes:
+  teslamate-db:
+  teslamate-grafana-data:
+  mosquitto-conf:
+  mosquitto-data:" > docker-compose.yml
+
