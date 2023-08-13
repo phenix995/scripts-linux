a1f8c6d7c4f5a4f3c74fcbe26346bf51b1c6528b
Mathieu <44148051+phenix995@users.noreply.github.com>
2023-08-13 15:48:39 -0400

    Update install-vm-basic-debian.sh

--- install-vm-basic-debian.sh
+++ install-vm-basic-debian.sh
@@ -1,10 +1,6 @@
 #!/bin/bash
 #run as root
-<<<<<<<< HEAD:install-vm-basic-debian.sh
 source dist-upgrade-apt.sh
-========
-
->>>>>>>> cba813f108213feffa1c6869872c3c4434e59b1c:install-vm-debian-basic.sh
 apt install -y sudo
 apt install -y qemu-guest-agent qemu-utils
 systemctl start qemu-guest-agent
