4792447772ca4eaf5f205dee98f7d7a411623922
Mathieu <44148051+phenix995@users.noreply.github.com>
2023-08-13 16:35:43 -0400

    install docker

---
+++ install-docker.sh
@@ -0,0 +1,28 @@
+#!/bin/sh
+# https://gist.github.com/EvgenyOrekhov/1ed8a4466efd0a59d73a11d753c0167b
+
+set -o errexit
+set -o nounset
+
+IFS=$(printf '\n\t')
+
+# Docker
+apt remove --yes docker docker-engine docker.io containerd runc || true
+apt update
+apt --yes --no-install-recommends install apt-transport-https ca-certificates
+wget --quiet --output-document=- https://download.docker.com/linux/ubuntu/gpg | apt-key add -
+add-apt-repository --yes "deb [arch=$(dpkg --print-architecture)] https://download.docker.com/linux/ubuntu $(lsb_release --codename --short) stable"
+apt update
+apt --yes --no-install-recommends install docker-ce docker-ce-cli containerd.io
+usermod --append --groups docker "$USER"
+systemctl enable docker
+printf '\nDocker installed successfully\n\n'
+
+printf 'Waiting for Docker to start...\n\n'
+sleep 5
+
+# Docker Compose
+wget --output-document=/usr/local/bin/docker-compose "https://github.com/docker/compose/releases/download/$(wget --quiet --output-document=- https://api.github.com/repos/docker/compose/releases/latest | grep --perl-regexp --only-matching '"tag_name": "\K.*?(?=")')/run.sh"
+chmod +x /usr/local/bin/docker-compose
+wget --output-document=/etc/bash_completion.d/docker-compose "https://raw.githubusercontent.com/docker/compose/$(docker-compose version --short)/contrib/completion/bash/docker-compose"
+printf '\nDocker Compose installed successfully\n\n'
